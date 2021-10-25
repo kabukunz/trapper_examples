ef48ae4f989a397d82561f9793c30fa5c4664eb4
kabukunz <kabukunz@gmail.com>
2021-10-25 16:28:33 +0200

    example on windows

--- build.bat
+++ build.bat
@@ -7,9 +7,14 @@ rmdir /S /Q -rf mybuild
 rmdir /S /Q -rf myprebuilt
 rmdir /S /Q -rf mythirdparty
 
+rmdir /S /Q -rf .cache
+rmdir /S /Q -rf build
+rmdir /S /Q -rf prebuilt
+rmdir /S /Q -rf thirdparty
+
 @REM Build default examples
 
-call .\scripts\examples\default\mmg\build.bat
+call .\examples\default\mmg\build.bat
 @REM call build_embree.bat
 @REM call build_dlib.bat
 @REM call build_polyscope.bat
