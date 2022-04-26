12355a2e0074b1710ac27a9bcdbb3376122a888b
kabu <kabukunz@gmail.com>
2022-04-26 13:06:59 +0200

    ccchanges

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# rm -rf build
+
+# rm -rf .mycache
+# rm -rf mybuild
+# rm -rf myprebuilt
+# rm -rf mythirdparty
+
+# rm -rf .cache
+# rm -rf build
+# rm -rf prebuilt
+# rm -rf thirdparty
+
+# build default examples
+
+./examples/default/mmg/build.sh
+# ./examples/default/embree/build.sh
+# ./examples/default/dlib/build.sh
+# ./examples/default/imgui/build.sh
+# ./examples/default/pybind11/build.sh
+# ./build_polyscope.sh
+
+# build advanced examples
+
+# ./scripts/examples/advanced/mmg/build.sh
+# ./build_embree.sh
+# ./build_dlib.sh
+# ./build_polyscope.sh
+
+
